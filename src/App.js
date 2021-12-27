e8920aea574e2f3ce24eac57deccdbc43c31cf19
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-26 20:59:42 -0500

    converted function app to class app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    "RRRRRREEEEEEEEAAAAAACCCCCCCTTTTTTT"
-  );
+class App extends React.Component {
+  render() {
+    return (
+      "RRRRRREEEEEEEEAAAAAACCCCCCCTTTTTTT"
+    );
+  }
 }
 
 export default App;
