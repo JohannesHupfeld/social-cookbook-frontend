6d5995daa5c01e07e0308e9f2a122808d5dfc2cc
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-29 16:23:45 -0500

    place navbar and main components inside a class div name App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import  Navbar from './components/Navbar.js'
+import  Main from './components/Main.js'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './redux/actions/currentUser.js'
 
@@ -12,7 +13,10 @@ class App extends React.Component { // made a class to have componentDidMount
 
   render() {
     return (
-      <Navbar />
+      <div className='App'>      
+        <Navbar />
+        <Main /> 
+      </div>
     );
   }
 }
