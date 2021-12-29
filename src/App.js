27e865173b66919626323ea33537e76c56c430b8
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-29 14:03:55 -0500

    mapStateToProps current user object from store. if current user show logout else show login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import './App.css';
 import Login from './components/Login.js'
+import Logout from './components/Logout.js'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './redux/actions/currentUser.js'
 
-class App extends React.Component {
+class App extends React.Component { // made a class to have componentDidMount
 
   componentDidMount() {
     this.props.getCurrentUser()
-  }
+  } // gets my current user 
 
   render() {
     return (
-      // "RRRRRREEEEEEEEAAAAAACCCCCCCTTTTTTT"
-      <Login />
+      this.props.currentUser ? <Logout /> : <Login /> 
     );
   }
 }
 
-export default connect(null, { getCurrentUser })(App);
+const mapStateToProps = ({currentUser}) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
