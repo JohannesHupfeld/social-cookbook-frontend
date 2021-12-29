e9c70b98770e4d8306bdb347c0258896cbdd2be6
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-29 14:09:20 -0500

    added CLEAR_CURRENT_USER case

--- src/redux/reducers/currentUser.js
+++ src/redux/reducers/currentUser.js
@@ -2,6 +2,10 @@ function currentUserReducer(state = null, action) { // state null for truthiness
   switch (action.type) {
     case "SET_CURRENT_USER":
       return action.user
+
+    case "CLEAR_CURRENT_USER":
+      return null
+      
     default:
       return state
   }
