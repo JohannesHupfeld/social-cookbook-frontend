5041e34350992bf3c9b1b7d43cd1af91bcd146e2
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-30 15:09:27 -0500

    RecipCard which exports to Recipe and displays each recipe

---
+++ src/components/RecipeCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const RecipeCard = ({ recipe }) => {
+  return (  
+    <div className='RecipeCard'> 
+      
+      <h2>{recipe.attributes.name}</h2>
+      <img src={recipe.attributes.image_url}></img>
+      <br></br>
+      <strong>Ingredients</strong>
+      <h3>{recipe.attributes.ingredients}</h3>
+      <strong>Instructions</strong>
+      <h3>{recipe.attributes.instructions}</h3>
+      <h4> created on {recipe.attributes.created_at}</h4>
+      
+    </div>
+  )
+} 
+
+export default RecipeCard 
