66cb7904be2a099e900224e211c77ee2417befcd
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-30 18:55:57 -0500

    removed main and added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './App.css';
 import  Navbar from './components/Navbar.js'
-import  Main from './components/Main.js'
+import  Login from './components/Login.js'
+import  Signup from './components/Signup.js'
+import  Recipes from './components/Recipes.js'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './redux/actions/currentUser.js'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 class App extends React.Component { // made a class to have componentDidMount
 
@@ -13,10 +17,14 @@ class App extends React.Component { // made a class to have componentDidMount
 
   render() {
     return (
-      <div className='App'>      
-        <Navbar /> 
-        <Main /> 
-      </div> 
+      <Router>
+        <div className='App'>      
+          <Navbar /> 
+          <Route exact path='/signup' component={Signup}/>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/recipes' component={Recipes}/>
+        </div> 
+      </Router> 
     );
   }
 }
