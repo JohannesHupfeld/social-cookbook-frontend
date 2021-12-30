371281a4d3f0e66fe9d2aa8a5516e8537f62e62e
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-30 01:13:59 -0500

    finished GET fetch request

--- src/redux/actions/recipes.js
+++ src/redux/actions/recipes.js
@@ -1,3 +1,5 @@
+// SYNCH
+
 export const setRecipes = recipes => {
   return {
     type: "SET_RECIPES",
@@ -5,14 +7,35 @@ export const setRecipes = recipes => {
   }
 }
 
+
+// ASYNCH 
+
+// get recipes
 export const getRecipes = () => {
   return dispatch => {
     fetch("http://localhost:3001/api/v1/recipes", {
+      credentials: "include",
       method: "GET",
       headers: {
         "Content-Type":"application/json",
         "Accept":"application/json"
       }
     })
+    .then(resp => resp.json())
+    .then(recipes => {
+      if (recipes.error) {
+        alert(recipes.error)
+      } else {
+        dispatch(setRecipes(recipes.data))
+      }
+    })
+    .catch(console.log) 
   }
 }
+
+
+// postRecipes
+
+// editRecipes
+
+// deleteRecipes
