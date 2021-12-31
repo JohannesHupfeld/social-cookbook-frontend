6ef451c45efc90111e61eb0d9513e2b6ca291713
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-30 19:01:39 -0500

    created signup form from login template

---
+++ src/components/Signup.js
@@ -0,0 +1,42 @@
+// REDUX
+import React from 'react'
+import { connect } from 'react-redux'
+import { updateSignupForm } from '../redux/actions/signupForm.js'
+import { signup } from '../redux/actions/signupForm'
+
+const Signup = ({ signupForm, updateSignupForm, signup }) => { // arguments are state from store used as props
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target
+    const updateFormInfo = {
+      ...signupForm,
+      [name]: value
+    }
+    updateSignupForm(updateFormInfo)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    signup(signupForm)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input placeholder="name" value={signupForm.name} name="name" type="text" onChange={handleFormChange} /> 
+      <input placeholder="username" value={signupForm.username} name="username" type="text" onChange={handleFormChange} /> 
+      <input placeholder="password" value={signupForm.password} name="password" type="text" onChange={handleFormChange} /> 
+      <input type="submit" value="Signup" />
+    </form>
+  )
+}
+
+// this gives me an argument coming to this component that looks like this
+// {username: "joeyhup", password: "password"}
+const mapStateToProps = (state) => {
+  return {
+    signupForm: state.signupForm
+  }
+}
+
+export default connect(mapStateToProps, { updateSignupForm, signup} )(Signup) // arguments are state grabbed from redux store then used as props above
+// export default connect(mapStateToProps, { updateLoginForm: updateLoginForm })(Login)
