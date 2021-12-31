3b050a01cca86dc860ddfe3c8fe2645b5e05b8ed
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-30 19:02:24 -0500

    created signup post requests with action creators

---
+++ src/redux/actions/signupForm.js
@@ -0,0 +1,47 @@
+import { setCurrentUser } from './currentUser.js'
+import { getRecipes } from './recipes.js'
+
+// SYNCH
+export const resetSignupForm = () => { // lives in login 
+  return {
+    type: "RESET_SIGNUP_FORM"
+  }
+}
+
+export const updateSignupForm = (formData) => { // lives in login 
+  return {
+    type: "UPDATE_SIGNUP_FORM",
+    formData
+  }
+}
+
+
+
+// ASYNCH
+export const signup = (credentials) => {
+  return dispatch => {
+    const credentialInfo = {
+      user: credentials
+    }
+    fetch("http://localhost:3001/api/v1/signup", {
+      credentials: "include",
+      method: "POST", // sending data of a new user
+      headers: {
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+      },
+      body: JSON.stringify(credentialInfo)
+    })
+    .then(resp => resp.json())
+    .then(user => {
+      if (user.error) {
+        alert(user.error)
+      } else {
+        dispatch(setCurrentUser(user.data.attributes))
+        dispatch(getRecipes())
+        dispatch(resetSignupForm())
+      }
+    })
+    .catch(console.log)
+  }
+}
