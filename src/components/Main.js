96a4b1a834fe982df9901c3a69f80415e7fbfe86
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-31 13:10:43 -0500

    decomissioned for now

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import  Recipes from './Recipes.js'
+// import React from 'react'
+// import  Recipes from './Recipes.js'
+// import Login from '../components/Login.js'
+// import Signup from '../components/Signup.js'
+// import Home from '../components/Home.js'
+// import { Route } from 'react-router-dom'
+// import { connect } from 'react-redux'
 
-const Main = () => {
-  return (
-    <div className='Main'>
-       <Recipes />
-    </div>
-  )
-}
 
-export default Main
+// class Main extends React.Component {
+  
+  
+// render(currentUser) {
+//   const loggedIn = !!currentUser
+//   return (
+//     <div className='Main'>
+//       <Route exact path='/' render={() => loggedIn ? <Recipes /> : <Home />}/>
+//       <Route exact path='/signup' component={Signup}/>
+//       <Route exact path='/login' component={Login}/>
+//       <Route exact path='/recipes' component={Recipes}/>
+//       <Recipes />
+//     </div>
+//     )
+//   }
+// }
+
+// const mapStateToProps = ({currentUser}) => {
+//   return {
+//     currentUser
+//   }
+// }
+
+// export default connect(mapStateToProps)(Main)
