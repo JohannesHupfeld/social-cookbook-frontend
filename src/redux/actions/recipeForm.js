7b01ea394bebcb31f8d1517b3ff7fa03c4590b3d
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-02 17:29:43 -0500

    synchronous actions of the action file

---
+++ src/redux/actions/recipeForm.js
@@ -0,0 +1,16 @@
+//SYNCH
+export const resetRecipeForm = () => {
+  return {
+    type: "RESET_RECIPE_FORM"
+  }
+}
+
+export const updateRecipteForm = () => {
+  return {
+    type: "UPDATE_RECIPE_FORM" 
+  }
+}
+
+
+
+//ASYCH
