cb4d2aac725216d71df39fe66a9397f045948a5b
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-04 20:52:05 -0500

    created proper routes to signup login and recipes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React from 'react'
 import './App.css';
-import Navbar from './components/Navbar.js'
+// import Navbar from './components/Navbar.js'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './redux/actions/currentUser.js'
+import { Route, withRouter } from 'react-router-dom'
+import Signup from './components/Signup.js'
+import Recipes from './components/Recipes.js'
+import Login from './components/Login.js'
+import Main from './components/Main.js'
 class App extends React.Component { // made a class to have componentDidMount
 
   componentDidMount() {
     this.props.getCurrentUser()
   } // gets my current user 
-
+  
   render() {
+    const {loggedIn} = this.props
     return (
       <div className='App'> 
-        
-        <Navbar /> 
+        <Route exact path='/signup' component={Signup}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/' render={(props)=> loggedIn ? <Recipes {...props} /> :<Main {...props}/>}/>
+        <Route exact path='/recipes' component={Login}/>
+        {/* <Navbar /> */}
       </div> 
     );
   }
 }
 
-export default connect(null, { getCurrentUser })(App);
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser
+  })
+
+}
+
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
+
+// export default App
