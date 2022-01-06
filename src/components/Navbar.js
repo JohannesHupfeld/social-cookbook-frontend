f3ae0d6fa4fce4d76088a3f735d7a6a25b6c6432
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-05 23:58:27 -0500

    removed logic and added only links to recipe and new recipe and added logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,23 @@
 import React from 'react'
-import Login from './Login.js'
-import Logout from './Logout.js'
-import Signup from './Signup.js'
-import Recipes from './Recipes.js'
+import Logout from './Logout'
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
-
-const Navbar = ({ currentUser }) => { // currentUser comes from mapStateToProps
-
+const Navbar = ({ loggedIn }) => {
   return (
-   <div className='Navbar'>
-     { currentUser ? <h2>Welcome {currentUser.name}</h2> : <h2>Welcome to the CookBook App</h2>} 
-     { currentUser ? <Logout /> : <h3>Login Here<Login /></h3>} 
-     { currentUser ? <h3>Here are your Recipes<Recipes /></h3> : <h3>Signup Here<Signup /></h3>}
-     <Route exact path='/signup' component={Signup}/>
-     <Route exact path='/login' component={Login}/>
-     {/* <Route exact path='/recipes' component={Recipes}/> */}
-   </div>
+    <div className='Navbar'>
+      <NavLink to='/recipes'>Recipes</NavLink>
+      <NavLink to='/recipes/new'>New Recipe</NavLink> 
+      { loggedIn ? <Logout /> : null}   
+    </div>
   )
-} 
+}
 
-const mapStateToProps = ({currentUser}) => { // destructing because currentUser is an object. comes from redux store
-  return {
-    currentUser
-  }
+const mapStateToProps = ({currentUser}) => {
+  return ({
+    currentUser,
+    loggedIn: !!currentUser
+  })
 }
 
 export default connect(mapStateToProps)(Navbar)
-// if null isnt stated then itll look at it as the map state
