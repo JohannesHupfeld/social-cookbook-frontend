17fd5ccf0d610b30f23b5b73a4807f608af15808
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-06 17:07:11 -0500

    added div class

--- src/components/CreateRecipeForm.js
+++ src/components/CreateRecipeForm.js
@@ -20,17 +20,19 @@ const CreateRecipeForm = ({ recipeForm, updateCreateRecipeForm, createRecipe, hi
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input placeholder='name' value={recipeForm.name} name="name" type="text" onChange={handleFormChange} />
-      <br></br>
-      <input placeholder='image_url' value={recipeForm.image_url} name="image_url" type="text" onChange={handleFormChange} />
-      <br></br>
-      <textarea placeholder='ingredients' value={recipeForm.ingredients} name="ingredients" type="text_area" onChange={handleFormChange} />
-      <br></br>
-      <textarea placeholder='instructions' value={recipeForm.instructions} name="instructions" type="text_area" onChange={handleFormChange} />
-      <br></br>
-      <input type="submit" value="Create Recipe" />
-    </form>
+      <div className='Create-Recipe-Form'>
+        <form onSubmit={handleSubmit}>
+          <input placeholder='name' value={recipeForm.name} name="name" type="text" onChange={handleFormChange} />
+          <br></br>
+          <input placeholder='image_url' value={recipeForm.image_url} name="image_url" type="text" onChange={handleFormChange} />
+          <br></br>
+          <textarea placeholder='ingredients' value={recipeForm.ingredients} name="ingredients" type="text_area" onChange={handleFormChange} />
+          <br></br>
+          <textarea placeholder='instructions' value={recipeForm.instructions} name="instructions" type="text_area" onChange={handleFormChange} />
+          <br></br>
+          <input type="submit" value="Create Recipe" />
+        </form>
+      </div>
   )
 }
 
