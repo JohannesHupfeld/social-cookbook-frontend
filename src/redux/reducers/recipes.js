a663189a9f9913382331cff3cb91a582dcec5b30
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-06 00:03:38 -0500

    added two cases

--- src/redux/reducers/recipes.js
+++ src/redux/reducers/recipes.js
@@ -1,7 +1,15 @@
+// import Home from './components/Home.js'
+
 function recipeReducer(state = [], action) {
   switch (action.type) {
     case "SET_RECIPES":
       return action.recipes
+
+    case "CLEAR_RECIPES":
+      return []
+
+    case "CLEAR_RECIPE_FORM":
+      return []
 
     default:
       return state
