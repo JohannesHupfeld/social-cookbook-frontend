a60e62255c170bde36f0b84fd50d50abbccdd457
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-06 17:09:44 -0500

    route back to home and added div class

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,21 +2,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../redux/actions/loginForm.js'
+import { withRouter } from 'react-router-dom'
 
 const Logout = ({ logout, history }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
     logout()
-    history.push('/home')
-
+    history.push('/')
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="submit" value="Logout" />
-    </form>
+     <div className='Logout'>
+      <form onSubmit={handleSubmit}>
+        <input type="submit" value="Logout" />
+      </form>
+    </div>
   )
 }
 
-export default connect(null, { logout } )(Logout)
+export default withRouter(connect(null, { logout } )(Logout))
 // if null isnt stated then itll look at it as the map state
