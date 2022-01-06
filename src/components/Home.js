035842171de17ba66f2da8e1bfc11e873ebd44df
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-05 20:50:46 -0500

    moved code from main to home

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Login from './Login'
+// import Logout from './Logout'
+// import Signup from './Signup'
+import { Link } from 'react-router-dom'
+
+const Main = () => (
+  <div>
+    <h2>
+      <Link to='/signup'>Signup</Link>
+      <br></br>
+      or
+      <br></br>
+      <Link to='/login'>Login</Link>
+    </h2>
+  
+  </div>
+)
+
+export default Main
