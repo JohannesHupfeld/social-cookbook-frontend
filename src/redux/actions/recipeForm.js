316c084ee288378bb3b3e8d49fc85863e1ef2409
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-06 00:00:43 -0500

    renamed synchronous action creators and created my asynch action creator

--- src/redux/actions/recipeForm.js
+++ src/redux/actions/recipeForm.js
@@ -1,16 +1,49 @@
+ import { setRecipes } from './recipes'
 //SYNCH
-export const resetRecipeForm = () => {
+export const resetCreateRecipeForm = () => {
   return {
-    type: "RESET_RECIPE_FORM"
+    type: "RESET_CREATE_RECIPE_FORM"
   }
 }
 
-export const updateRecipteForm = () => {
+export const updateCreateRecipeForm = (formData) => { // formData is name & value
   return {
-    type: "UPDATE_RECIPE_FORM" 
+    type: "UPDATE_CREATE_RECIPE_FORM",
+    formData
   }
 }
 
 
 
 //ASYCH
+export const createRecipe = (recipeData, history) => {
+  return dispatch => {
+    const sendableRecipeData = {
+      name: recipeData.name,
+      image_url: recipeData.image_url,
+      ingredients: recipeData.ingredients,
+      instructions: recipeData.instructions
+    }
+    fetch("http://localhost:3001/api/v1/recipes", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+      },
+      body: JSON.stringify(sendableRecipeData)
+    })
+    .then(resp => resp.json())
+    .then(recipe => {
+      if (recipe.error) {
+        alert(recipe.error)
+      } else {
+        dispatch(setRecipes(recipe.data.attributes))
+        dispatch(resetCreateRecipeForm)
+        dispatch(updateCreateRecipeForm)
+        history.push('/')
+      }
+    })
+    .catch(console.log)
+  }
+}
