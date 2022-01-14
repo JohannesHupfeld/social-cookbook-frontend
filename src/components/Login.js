7d2cb77cd4fdb18a920dab4aa8eecb26bc00bad3
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-14 17:53:25 -0500

    styling changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,16 +21,24 @@ const Login = ({ loginForm, updateLoginForm, login, history}) => {
     login(loginForm, history) // histroy is a mutable object
   }
 
+  function handleClick(){ // this and the button should be in its own component possibly in presentational folder 
+    console.log("click")
+    history.goBack()
+  }
+
   return (
     <div className='Login'>
       <form onSubmit={handleSubmit}>
-        <h4>Please login below </h4>
+        <h4>Please Login Below </h4>
         <input placeholder="username" value={loginForm.username} name="username" type="text" onChange={handleFormChange} />
         <input placeholder="password" value={loginForm.password} name="password" type="text" onChange={handleFormChange} />
         <input type="submit" value="Login" />
         <br></br>
         <br></br>
         <Link to="/signup" className='login-to-signup'> or Signup</Link>
+        <br></br>
+        <br></br>
+        <button onClick={handleClick}>Back</button> 
       </form>
     </div>
   )
