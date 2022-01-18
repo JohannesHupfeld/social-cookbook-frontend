76c289f80754f71a4e246230d8cdb10a9e187a69
JohannesHupfeld <j.hupfeld@gmail.com>
2022-01-17 21:16:42 -0500

    logo instead of null for ternary

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import { connect } from 'react-redux'
 import Main from './components/Main.js'
 import Navbar from './components/Navbar.js';
 // import Home from './components/Home';
+import Logo from './components/Logo.js'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+{/* <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+  crossorigin="anonymous"
+/> */}
 
 const App = ({ loggedIn }) => {
     return (
       <div className='App'> 
-        { loggedIn ? <Navbar /> : null} 
+        { loggedIn ? <Navbar /> : <Logo />} 
         <Main />
       </div> 
     );
